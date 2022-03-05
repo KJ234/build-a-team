ec7f2004b1f8b3e73854c2d9b1fcfb1e3f6a155b
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-03-05 20:00:28 +0000

    test

--- __tests__/intern.test.js
+++ __tests__/intern.test.js
@@ -1,1 +1,9 @@
 const Intern = require("../lib/Intern");
+
+it("Should return previous school ", () => {
+  expect(intern.getSchool()).toBe("Dar town Academy");
+});
+
+it("Should return the intern name when requested", () => {
+  expect(intern.getName()).toBe(intern);
+});
