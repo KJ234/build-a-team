1252b3862c612f190f06eb4eac7cdec038173f40
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-03-05 15:48:52 +0000

    specifying paths

---
+++ index.js
@@ -0,0 +1,8 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const Manager = require("./lib/Manager.js");
+const Engineer = require("./lib/Engineer.js");
+const Intern = require("./lib/Intern.js");
+
+const generatePage = require("./src/generatePage");
